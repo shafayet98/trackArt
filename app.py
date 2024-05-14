951c9a7de555df503941536fe16e6511bcfb174c
shafayet <shafayet.islam.61@gmail.com>
2024-05-14 20:32:51 +0930

    template introduction

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ from openai import OpenAI
 import json
 import time
 
+app = Flask(__name__)
+API_KEY = open("API_KEY", 'r').read()
+
+
+client = OpenAI(api_key=API_KEY)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', data="Hello World")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
